bab821f9faba7ff02c5a6e5ebcd473530151fc2d
Sonali Sridhar <sonali.sridhar@gmail.com>
2012-10-11 12:55:18 -0400

    Took out the playagain(); function. Felt it was an over kill

--- tictactoe.js
+++ tictactoe.js
@@ -96,18 +96,7 @@ function checkForWinners(symbol){
     for(var a = 0; a < winningCombinations.length; a++){
         if(content[winningCombinations[a][0]] == symbol && content[winningCombinations[a][1]] == symbol && content[winningCombinations[a][2]] == symbol){
             alert(symbol+ "won!");
-            playAgain();
+            location.reload(true);
         }
-    }
-}
-
-function playAgain(){
-    y = confirm("Wanna Play Again?");
-    if (y == true){
-        alert("Okay!");
-        location.reload(true);
-    }
-    else{
-        alert("Bye!");
     }
 }
